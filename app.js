93164430f07d05be1c07eeac08e641294a696568
Akbor5374 <akborhossain53741@gmail.com>
2020-08-16 20:15:55 +0600

    update done

--- app.js
+++ app.js
@@ -1,22 +1,4 @@
-//dom select
-//global
-const wNewConfirmed = document.querySelector(".wNewConfirmed");
-const wTotalConfirmed = document.querySelector(".wTotalConfirmed");
-const wNewDeaths = document.querySelector(".wNewDeaths");
-const wTotalDeaths = document.querySelector(".wTotalDeaths");
-const wNewRecovered = document.querySelector(".wNewRecovered");
-const wTotalRecovered = document.querySelector(".wTotalRecovered");
-
-//bd
-const bNewConfirmed = document.querySelector(".bNewConfirmed");
-const bTotalConfirmed = document.querySelector(".bTotalConfirmed");
-const bNewDeaths = document.querySelector(".bNewDeaths");
-const bTotalDeaths = document.querySelector(".bTotalDeaths");
-const bNewRecovered = document.querySelector(".bNewRecovered");
-const bTotalRecovered = document.querySelector(".bTotalRecovered");
-
 //api call
-
 const api = `https://api.covid19api.com/summary?fbclid=IwAR2YmZZfer5buethbbCblBK_LoFfACaLOgfOMB3Jtiv5iy0RucdM_BXpaFU`;
 
 fetch(api)
@@ -26,20 +8,20 @@ fetch(api)
     // show display
      function displayShow(data){
         //global info
-        wNewConfirmed.innerText = data.Global.NewConfirmed;
-        wTotalConfirmed.innerText = data.Global.TotalConfirmed;
-        wNewDeaths.innerText = data.Global.NewDeaths;
-        wTotalDeaths.innerText = data.Global.TotalDeaths;
-        wNewRecovered.innerText = data.Global.NewRecovered;
-        wTotalRecovered.innerText = data.Global.TotalRecovered;
+        document.querySelector(".wNewConfirmed").innerText = data.Global.NewConfirmed;
+        document.querySelector(".wTotalConfirmed").innerText = data.Global.TotalConfirmed;
+        document.querySelector(".wNewDeaths").innerText = data.Global.NewDeaths;
+        document.querySelector(".wTotalDeaths").innerText = data.Global.TotalDeaths;
+        document.querySelector(".wNewRecovered").innerText = data.Global.NewRecovered;
+        document.querySelector(".wTotalRecovered").innerText = data.Global.TotalRecovered;
 
         //bd info
-        bNewConfirmed.innerText = data.Countries[13].NewConfirmed;
-        bTotalConfirmed.innerText = data.Countries[13].TotalConfirmed;
-        bNewDeaths.innerText = data.Countries[13].NewDeaths;
-        bTotalDeaths.innerText = data.Countries[13].TotalDeaths;
-        bNewRecovered.innerText = data.Countries[13].NewRecovered;
-        bTotalRecovered.innerText = data.Countries[13].TotalRecovered;
+        document.querySelector(".bNewConfirmed").innerText = data.Countries[13].NewConfirmed;
+        document.querySelector(".bTotalConfirmed").innerText = data.Countries[13].TotalConfirmed;
+        document.querySelector(".bNewDeaths").innerText = data.Countries[13].NewDeaths;
+        document.querySelector(".bTotalDeaths").innerText = data.Countries[13].TotalDeaths;
+        document.querySelector(".bNewRecovered").innerText = data.Countries[13].NewRecovered;
+        document.querySelector(".bTotalRecovered").innerText = data.Countries[13].TotalRecovered;
      }
 
      // reload window 
